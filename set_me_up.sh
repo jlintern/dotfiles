1fb867ee2a428b88e4f3d5a57c77edd9bd449031
James Lintern <jameslintern@gmail.com>
2014-02-13 19:24:31 -0500

    fix directory references in set_me_up.sh

--- set_me_up.sh
+++ set_me_up.sh
@@ -1,7 +1,5 @@
-
 echo "Cloning public oh-my-zsh repository..."
 git clone git://github.com/robbyrussell/oh-my-zsh.git ~/.oh-my-zsh
-
 
 # if profile was specified on the command line
 if [ -n "$2" ]; then
@@ -12,16 +10,16 @@ else
   read -p "Please choose a profile." profile
 fi
 
-if [ ! -d ~/.dotfiles/$profile/ ]; then
-  echo "~/.dotfiles/$profile/ is not a directory, quitting..."
+if [ ! -d ~/.dotfiles/profiles/$profile/ ]; then
+  echo "~/.dotfiles/profiles/$profile/ is not a directory, quitting..."
   exit 1
 fi
 
-if [ ! -a ~/.dotfiles/$profile/.zshrc ]; then
-  echo "~/.dotfiles/$profile/.zshrc not found, quitting..."
+if [ ! -a ~/.dotfiles/profiles/$profile/.zshrc ]; then
+  echo "~/.dotfiles/profiles/$profile/.zshrc not found, quitting..."
 fi
-echo "Copying ~/.dotfiles/$profile/.zshrc to home directory"
-cp ~/.dotfiles/$profile/.zshrc ~/.zshrc
+echo "Copying ~/.dotfiles/profiles/$profile/.zshrc to home directory"
+cp ~/.dotfiles/profiles/$profile/.zshrc ~/.zshrc
 
 echo "Changing shell to /bin/zsh"
 chsh -s /bin/zsh
