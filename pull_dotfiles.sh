0db7f9b17529f989b636cd2072eb3c44aa172892
James Lintern <jameslintern@gmail.com>
2014-02-24 13:30:13 -0500

    in push/pull_dotfiles.sh, route pushd and popd to /dev/null

--- pull_dotfiles.sh
+++ pull_dotfiles.sh
@@ -3,8 +3,8 @@
 # this file is copied into the user's home directory by set_me_up.sh
 
 ### dotfiles_substitution: define_repo_dir ###
-pushd $repo_dir
+pushd $repo_dir &> /dev/null
 git stash
 git pull --rebase
 ./set_me_up.sh
-popd
+popd &> /dev/null
--- push_dotfiles.sh
+++ push_dotfiles.sh
@@ -3,9 +3,9 @@
 # this file is copied into the user's home directory by set_me_up.sh
 
 ### dotfiles_substitution: define_repo_dir ###
-pushd $repo_dir
+pushd $repo_dir &> /dev/null
 git commit -a
 git pull --rebase
 git push
 ./set_me_up.sh
-popd
+popd &> /dev/null
