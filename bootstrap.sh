671ba69cabf9b487f2850b181abdcd479874991d
James Lintern <jameslintern@gmail.com>
2014-04-21 21:05:36 -0400

    use ssh instead of git protocol

--- bootstrap.sh
+++ bootstrap.sh
@@ -8,5 +8,5 @@ else
 fi
 
 echo "Cloning private dotfiles repository..."
-ssh -t $host "git clone git://github.com/jlintern/dotfiles.git ~/.dotfiles"
+ssh -t $host "git clone ssh://git@github.com/jlintern/dotfiles.git ~/.dotfiles"
 ssh -t $host "bash ~/.dotfiles/set_me_up.sh personal"
